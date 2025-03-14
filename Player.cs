b7b6dc79f4b55f0aaaec71d0d2e8d795cc81358a
FabriDamazio <fabridamazio@gmail.com>
2025-03-13 22:29:03 -0300

    Basic movement

---
+++ Player.cs
@@ -0,0 +1,21 @@
+using Godot;
+
+public partial class Player : Node
+{
+    [Export]
+    private Character _character;
+
+    [Export]
+    private Camera3D _camera;
+
+    private Vector2 _inputDirection;
+    private Vector3 _moveDirection;
+
+    public override void _Process(double delta)
+    {
+        _inputDirection = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
+        _moveDirection = _camera.Basis.X * new Vector3(1, 0, 1) * _inputDirection.X;
+        _moveDirection += _camera.Basis.Z * new Vector3(1, 0, 1) * _inputDirection.Y;
+        _character.Move(_moveDirection);
+    }
+}
